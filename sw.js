ae5e7a9e0bb593b0476785854b62ebefb151b55a
Debbie <laicarrie06@gmail.com>
2026-08-14 17:33:22 +0800

    Prefer local photo copies before cloud downloads

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v10';
+const CACHE = 'little-food-diary-v11';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
