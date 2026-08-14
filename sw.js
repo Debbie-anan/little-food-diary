405f6591d67cfdd92441f527cda770d54b7d39e3
Debbie <laicarrie06@gmail.com>
2026-08-14 15:40:08 +0800

    Add private Supabase photo sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v5';
+const CACHE = 'little-food-diary-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
