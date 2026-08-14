e1b080999e029a0d2b324c969c146f9e0f7e5774
Debbie <laicarrie06@gmail.com>
2026-08-14 17:02:57 +0800

    Add password recovery for email-link accounts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v7';
+const CACHE = 'little-food-diary-v8';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
