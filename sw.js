ba86398351b59d7c9c67bdee8f02ac35c8ac5703
Debbie <laicarrie06@gmail.com>
2026-08-14 17:37:11 +0800

    Refine camera capture and custom moment time

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v11';
+const CACHE = 'little-food-diary-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
