c2400e3982a319c6ce0f756fc5fd03189f53eca6
Debbie <laicarrie06@gmail.com>
2026-08-14 15:51:04 +0800

    Add persistent email password login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v6';
+const CACHE = 'little-food-diary-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
