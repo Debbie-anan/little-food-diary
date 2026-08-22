dd70a4e44e11d9aa047b2beb427833bef3d88a6c
Debbie <laicarrie06@gmail.com>
2026-08-22 17:34:11 +0800

    Fix standalone Android PWA installation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'little-food-diary-v12';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'little-food-diary-v13';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
