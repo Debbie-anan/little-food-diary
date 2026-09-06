4fe8ddf3189ab21fe5b481aabcb13cc9c101d9e2
Debbie <laicarrie06@gmail.com>
2026-09-06 17:19:13 +0800

    Show each diary day as compact photo album

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v13';
+const CACHE = 'little-food-diary-v14';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
