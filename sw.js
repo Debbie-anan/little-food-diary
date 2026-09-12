302958e23c04d45d8fcdcb2c041ea721492de541
Debbie <laicarrie06@gmail.com>
2026-09-12 17:44:27 +0800

    Rename app to Memorable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v17';
+const CACHE = 'little-food-diary-v18';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
