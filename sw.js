ceaa8d03e4af5ccfc3981d840922c8ee99a93dbb
Debbie <laicarrie06@gmail.com>
2026-09-12 18:23:26 +0800

    Allow dragging collage photos and remember page positions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v21';
+const CACHE = 'little-food-diary-v22';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
