635094f39bde53c23aa980370495886b3fc2c28f
Debbie <laicarrie06@gmail.com>
2026-09-12 18:04:49 +0800

    Arrange week and month photos on a single collage page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v19';
+const CACHE = 'little-food-diary-v20';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
