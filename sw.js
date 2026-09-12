bdaa973941c20b27e0ddb750d6abd01eeaad4d26
Debbie <laicarrie06@gmail.com>
2026-09-12 18:15:57 +0800

    Use organic scattered photo layout and move delete into preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v20';
+const CACHE = 'little-food-diary-v21';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
