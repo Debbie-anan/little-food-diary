57f440b4e5b543318646f4e8e945f27025998b02
Debbie <laicarrie06@gmail.com>
2026-09-13 18:39:02 +0800

    Match notebook style with vertically stacked calendar and clock line icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'little-food-diary-v26';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './calendar-sticker.jpg', './clock-transparent.png'];
+const CACHE = 'little-food-diary-v27';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
