69df52dc4fb68ca8fcc2c02e5267e83c5cca0383
Debbie <laicarrie06@gmail.com>
2026-09-13 18:45:14 +0800

    Remove bulk clear records action

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'little-food-diary-v27';
+const CACHE = 'little-food-diary-v28';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
